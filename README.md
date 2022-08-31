61fb2416c6f62275a9ecfef47c518636ab6a0bff
Shuiabu Ibrahim <shuyibrahim@gmail.com>
2022-08-31 17:26:42 +0100

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+export365
+==================
+
+This app was initialized with [truffle init]
+
+
+==================
+export365 a decentralized repository for exporters and Importers across West-African region to connect them with the international trade market.
+
---
+++ contracts/Migrations.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Migrations {
+  address public owner = msg.sender;
+  uint public last_completed_migration;
+
+  struct exporter{
+    string name;
+    string decription;
+    string country;
+    string website;
+    string email;
+    address exporter_id;
+
+  }
+
+  modifier restricted() {
+    require(
+      msg.sender == owner,
+      "This function is restricted to the contract's owner"
+    );
+    _;
+  }
+
+  function setCompleted(uint completed) public restricted {
+    last_completed_migration = completed;
+  }
+
+  function addExpoter() public restricted{
+    
+  }
+
+}
